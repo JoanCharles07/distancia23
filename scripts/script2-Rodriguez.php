1888513a30c017073a3f9b43514b82be0fb90453
Juan Carlos <joancharles07@hotmail.com>
2023-05-21 16:46:37 +0200

    Modificacion de version distancia23

--- scripts/script2-Rodriguez.php
+++ scripts/script2-Rodriguez.php
@@ -2,15 +2,17 @@
 /**
 *Segundo ejemplo de documentación de PHP clase trabajador
 *@autor Juan Carlos Rodríguez Miranda
-*@version 1.0.1.
+*@version 1.1
 *@copyright Public Domain
 *@since 1.0.1
 */
 class trabajador{
 /**
 *@var string $nombre variable donde se almacenará el nombre
+*@var string $apellido variable donde se almacenara el apellido
 */
 	public $nombre="";
+	public $apellido="";
 /**
 *Método getNombre(uso este tipo porque es un ejemplo sino usaria __get)
 *esta función devuelve el nombre del trabajador.
@@ -26,6 +28,22 @@ class trabajador{
 	public function setNombre($nuevoNombre){
 		$this->nombre = $nuevoNombre;
 	}
+/**
+*Método getApellido
+*esta función devuelve el nombre del trabajador.
+*@return string con el apellido del trabajador.
+*/
+        public function getApellido(){            
+                return $this->apellido;
+        }
+/**
+*Metodo setApellido, esta función da un nuevo apellido al trabajador.
+*@param string nuevoApellido con el nuevo apellido del trabajador
+*/
+        public function setNombre($nuevoApellido){
+                $this->apellido = $nuevoApellido;
+        }
+
 }
 
 ?>
